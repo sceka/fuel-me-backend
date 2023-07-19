c03189688d06033defcee8d6fd4a47d431063175
sceka <scekicm02@gmail.com>
2023-07-19 14:05:59 +0200

    Add person model

---
+++ src/models/person.model.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+
+export type Person = {
+    name: string;
+    surname: string;
+    city: string;
+    address: string;
+    phone: string;
+};
+
+const personSchema = new Schema<Person>(
+    {
+        name: { type: String, required: true },
+        surname: { type: String, required: true },
+        city: { type: String, required: true },
+        address: { type: String, required: true },
+        phone: { type: String, required: true },
+    },
+    { timestamps: { createdAt: true, updatedAt: false } }
+);
